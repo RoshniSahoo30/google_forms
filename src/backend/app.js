991113b5980544c071ddbc1d8b63e79255103657
Roshni <roshnisahoo04@gmail.com>
2025-07-14 19:05:36 +0530

    backend

---
+++ src/backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+ const app= express();
+
+var cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+
+app.post('/form-submit', (req, res) => {
+    const formData = req.body;
+    console.log('Received form data:', formData);
+
+    res.status(200).json({ message: 'Form data received successfully!' });
+});
+
+app.listen(9000, () => {
+    console.log('Server is running on port 9000');
+})
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './components/StateProvider';
+import Reducer, { initialState } from './components/Reducer';
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StateProvider initialState={initialState} reducer={Reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
